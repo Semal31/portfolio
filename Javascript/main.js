2a2a30ee35b2c7ba821291c8dcb4c76c88acbd8b
semal31 <semal31@outlook.com>
2018-04-24 21:52:33 -0500

    javascript

---
+++ Javascript/main.js
@@ -0,0 +1,9 @@
+// Smooth Scroll //
+
+$(document).on('click', 'a[href^="#"]', function (event) {
+  event.preventDefault();
+
+  $('html, body').animate({
+    scrollTop: $($.attr(this, 'href')).offset().top
+  }, 700);
+})
